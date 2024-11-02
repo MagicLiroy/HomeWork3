8680a58091632624c7d57c02c0c9f25260dc9b87
kiril <kiril@DESKTOP-E9KJUIS>
2024-11-02 10:51:02 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение координат первой точки x1 и y1: ");
+            int x1 = int.Parse(Console.ReadLine());
+            int y1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение координат второй точки x2 и y2: ");
+            int x2 = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение координат третей точки x3 и y3: ");
+            int x3 = int.Parse(Console.ReadLine());
+            int y3 = int.Parse(Console.ReadLine());
+
+            int x4, y4;
+
+            if (x1 == x3 && y2 == y3 || x2 == x3 && y1 == y3 || x1 == x3 && y1 == y2 || x2 == x3 && y1 == y2 || x1 == x2 && y2 == y3 || x1 == x2 && y1 == y2)
+            {
+                if (x1 == x2 && y2 == y3)
+                {
+                    x4 = x3;
+                    y4 = y1;
+                    Console.WriteLine($"Координаты вершины: x = {x4}, y = {y4}");
+                }
+                else if (x1 == x2 && y1 == y3)
+                {
+                    x4 = x3;
+                    y4 = y2;
+                    Console.WriteLine($"Координаты вершины: x = {x4}, y = {y4}");
+                }
+                else if (x2 == x3 && y1 == y2)
+                {
+                    x4 = x1;
+                    y4 = y3;
+                    Console.WriteLine($"Координаты вершин: x = {x4}, y = {y4}");
+                }
+                else if (x1 == x3 && y2 == y3)
+                {
+                    x4 = x2;
+                    y4 = y1;
+                    Console.WriteLine($"Координаты вершин: x = {x4}, y = {y4}");
+                }
+                else if (x2 == x3 && y1 == y3)
+                {
+                    x4 = x1;
+                    y4 = y2;
+                    Console.WriteLine($"Координаты вершин: x = {x4}, y = {y4}");
+                }
+                else
+                {
+                    Console.WriteLine("Стороны не параллельны координатным осям!");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Укажите год в диапазоне  20 - 69: ");
+            int age = int.Parse(Console.ReadLine());
+
+            int unit = age % 10;
+            int decade = age / 10;
+
+            string decadeString, unitString;
+
+            if (age < 20 || age > 69)
+            {
+                Console.WriteLine("Введено неверное значение возраста");
+            }
+            else
+            {
+                switch (decade)
+                {
+                    case 2:
+                        decadeString = "Двадцать";
+                        switch (unit)
+                        {
+                            case 1:
+                                unitString = "один";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} год");
+                                break;
+                            case 2:
+                                unitString = "два";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} года");
+                                break;
+                            case 3:
+                                unitString = "три";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} года");
+                                break;
+                            case 4:
+                                unitString = "четыре";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} года");
+                                break;
+                            case 5:
+                                unitString = "пять";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 6:
+                                unitString = "шесть";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 7:
+                                unitString = "семь";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 8:
+                                unitString = "восемь";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 9:
+                                unitString = "девять";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 0:
+                                Console.WriteLine($"Ваш возраст: {decadeString} лет");
+                                break;
+                        }
+                        break;
+                    case 3:
+                        decadeString = "Тридцать";
+                        switch (unit)
+                        {
+                            case 1:
+                                unitString = "один";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} год");
+                                break;
+                            case 2:
+                                unitString = "два";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} года");
+                                break;
+                            case 3:
+                                unitString = "три";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} года");
+                                break;
+                            case 4:
+                                unitString = "четыре";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} года");
+                                break;
+                            case 5:
+                                unitString = "пять";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 6:
+                                unitString = "шесть";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 7:
+                                unitString = "семь";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 8:
+                                unitString = "восемь";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 9:
+                                unitString = "девять";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 0:
+                                Console.WriteLine($"Ваш возраст: {decadeString} лет");
+                                break;
+                        }
+                        break;
+                    case 4:
+                        decadeString = "Сорок";
+                        switch (unit)
+                        {
+                            case 1:
+                                unitString = "один";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} год");
+                                break;
+                            case 2:
+                                unitString = "два";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} года");
+                                break;
+                            case 3:
+                                unitString = "три";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} года");
+                                break;
+                            case 4:
+                                unitString = "четыре";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} года");
+                                break;
+                            case 5:
+                                unitString = "пять";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 6:
+                                unitString = "шесть";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 7:
+                                unitString = "семь";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 8:
+                                unitString = "восемь";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 9:
+                                unitString = "девять";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 0:
+                                Console.WriteLine($"Ваш возраст: {decadeString} лет");
+                                break;
+                        }
+                        break;
+                    case 5:
+                        decadeString = "Пятдесят";
+                        switch (unit)
+                        {
+                            case 1:
+                                unitString = "один";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} год");
+                                break;
+                            case 2:
+                                unitString = "два";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} года");
+                                break;
+                            case 3:
+                                unitString = "три";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} года");
+                                break;
+                            case 4:
+                                unitString = "четыре";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} года");
+                                break;
+                            case 5:
+                                unitString = "пять";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 6:
+                                unitString = "шесть";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 7:
+                                unitString = "семь";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 8:
+                                unitString = "восемь";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 9:
+                                unitString = "девять";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 0:
+                                Console.WriteLine($"Ваш возраст: {decadeString} лет");
+                                break;
+                        }
+                        break;
+                    case 6:
+                        decadeString = "Шестьдесят";
+                        switch (unit)
+                        {
+                            case 1:
+                                unitString = "один";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} год");
+                                break;
+                            case 2:
+                                unitString = "два";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} года");
+                                break;
+                            case 3:
+                                unitString = "три";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} года");
+                                break;
+                            case 4:
+                                unitString = "четыре";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} года");
+                                break;
+                            case 5:
+                                unitString = "пять";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 6:
+                                unitString = "шесть";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 7:
+                                unitString = "семь";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 8:
+                                unitString = "восемь";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 9:
+                                unitString = "девять";
+                                Console.WriteLine($"Ваш возраст: {decadeString} {unitString} лет");
+                                break;
+                            case 0:
+                                Console.WriteLine($"Ваш возраст: {decadeString} лет");
+                                break;
+                        }
+                        break;
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
